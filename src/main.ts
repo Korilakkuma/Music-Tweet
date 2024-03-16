33c9704a4e390e6260691f93fd6648c16f932bf0
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-03-16 17:25:15 +0900

    feat: Visualizer

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,44 @@ export const scriptContainer = () => {
       return;
     }
 
+    const svg: SVGSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+    svg.setAttribute('width', window.innerWidth.toString(10));
+    svg.setAttribute('height', window.innerHeight.toString(10));
+
+    svg.removeAttribute('hidden');
+
+    document.body.appendChild(svg);
+
     window.X('oscillator').setup([true, true, true, true]);
 
-    window.X('mml').setup();
+    window.X('mml').setup({
+      endedCallback: () => {
+        svg.removeAttribute('hidden');
+      },
+      errorCallback: () => {
+        svg.removeAttribute('hidden');
+      }
+    });
+
+    window
+      .X('oscillator')
+      .module('analyser')
+      .domain('time')
+      .setup(svg)
+      .activate()
+      .param({
+        interval: -1,
+        styles: {
+          wave: '#7ac4f7',
+          grid: 'none',
+          text: 'none',
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0
+        }
+      });
 
     window.setTimeout(() => {
       if (!window.X) {
